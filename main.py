1bf0653218be2d2cb52ffdecc727a3503b11a951
Nik Pirc <nik@oaa.si>
2021-10-22 16:14:53 +0200

    tracking the name and the secret number

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import datetime
 import json
 import random
 
+name = input("Please enter your name: ")
 secret = random.randint(1, 30)
 attempts = 0
 
@@ -10,14 +11,14 @@ with open("score_list.json", "r") as score_file:
     print("Top scores: " + str(score_list))
 
 for score_dict in score_list:
-    print(str(score_dict["attempts"]) + " attempts, date: " + score_dict.get("date"))
+    print(str("User {0} took {1} attempts with the secret number {2} on {3}".format(score_dict["name"], score_dict["attempts"], score_dict.get("secret"), score_dict.get("date"))))
 
 while True:
     guess = int(input("Guess the secret number (between 1 and 30): "))
     attempts += 1
 
     if guess == secret:
-        score_list.append({"attempts": attempts, "date": str(datetime.datetime.now())})
+        score_list.append({"name": name, "attempts": attempts,"secret": secret, "date": str(datetime.datetime.now())})
 
         with open("score_list.json", "w") as score_file:
             score_file.write(json.dumps(score_list))
