84bb8840f2bed044a61f9b8a5f9d213f60f76d3e
Nik Pirc <nik@oaa.si>
2021-10-26 14:38:27 +0200

    implemented functions

---
+++ functions.py
@@ -0,0 +1,54 @@
+import datetime
+import json
+import random
+
+def play_game(mode="easy"):
+    name = input("Please enter your name: ")
+    if mode.lower() == "hard":
+        secret = random.randint(1, 100)
+    elif mode.lower() == "easy":
+        secret = random.randint(1, 30)
+
+    attempts = 0
+    wrong_guesses = []
+
+    while True:
+        if mode.lower() == "hard":
+            guess = int(input("Guess the secret number (between 1 and 100): "))
+        elif mode.lower() == "easy":
+            guess = int(input("Guess the secret number (between 1 and 30): "))
+
+        attempts += 1
+
+        with open("score_list.json", "r") as score_file:
+            score_list = json.loads(score_file.read())
+
+        if guess == secret:
+            score_list.append(
+                {"name": name, "attempts": attempts, "secret": secret, "mode": mode, "date": str(datetime.datetime.now()),
+                 "wrong_guesses": wrong_guesses})
+
+            with open("score_list.json", "w") as score_file:
+                score_file.write(json.dumps(score_list))
+
+            print("You've guessed it - congratulations! It's number " + str(secret))
+            print("Attempts needed: " + str(attempts))
+            break
+        elif guess > secret:
+            print("Your guess is not correct... try something smaller")
+            wrong_guesses.append(guess)
+
+        elif guess < secret:
+            print("Your guess is not correct... try something bigger")
+            wrong_guesses.append(guess)
+
+def get_top_scores(number):
+    with open("score_list.json", "r") as score_file:
+        score_list = json.loads(score_file.read())
+
+    new_score_list = sorted(score_list, key=lambda k: k['attempts'])[:number]
+
+    for score_dict in new_score_list:
+        print(str("User {0} took {1} attempts in gamemode {5} with the secret number {2} on {3} \nAll the wrong guesses: {4}".format(
+            score_dict["name"], score_dict["attempts"], score_dict.get("secret"), score_dict.get("date"),
+            score_dict["wrong_guesses"], score_dict["mode"])))
--- main.py
+++ main.py
@@ -1,36 +1,16 @@
-import datetime
-import json
-import random
-
-name = input("Please enter your name: ")
-secret = random.randint(1, 30)
-attempts = 0
-wrong_guesses = []
-
-with open("score_list.json", "r") as score_file:
-    score_list = json.loads(score_file.read())
-
-new_score_list = sorted(score_list, key=lambda k: k['attempts'])[:3]
-
-for score_dict in new_score_list:
-    print(str("User {0} took {1} attempts with the secret number {2} on {3} \nAll the wrong guesses: {4}".format(score_dict["name"], score_dict["attempts"], score_dict.get("secret"), score_dict.get("date"), score_dict["wrong_guesses"])))
+from functions import play_game, get_top_scores
 
 while True:
-    guess = int(input("Guess the secret number (between 1 and 30): "))
-    attempts += 1
+    selection = input("Would you like to play a new game (write A), see the best scores (write B), or quit (write C)? ")
 
-    if guess == secret:
-        score_list.append({"name": name, "attempts": attempts,"secret": secret, "date": str(datetime.datetime.now()), "wrong_guesses": wrong_guesses})
-
-        with open("score_list.json", "w") as score_file:
-            score_file.write(json.dumps(score_list))
-
-        print("You've guessed it - congratulations! It's number " + str(secret))
-        print("Attempts needed: " + str(attempts))
+    if selection.upper() == "A":
+        mode = input("What mode do you want (hard or easy)? ")
+        if mode.lower() == "hard":
+            play_game("hard")
+        elif mode.lower() == "easy":
+            play_game("easy")
+    elif selection.upper() == "B":
+        number = int(input("How many top scores would you like to see? "))
+        get_top_scores(number)
+    elif selection.upper() == "C":
         break
-    elif guess > secret:
-        print("Your guess is not correct... try something smaller")
-        wrong_guesses.append(guess)
-    elif guess < secret:
-        print("Your guess is not correct... try something bigger")
-        wrong_guesses.append(guess)
